91ad8b141838bd9c9a3f30d5c8b02f4cf3fb2aca
Bhinsar <bhinsarjagdish2003@gmail.com>
2025-12-26 19:37:07 +0530

    feat: Add Prisma client configuration for PostgreSQL database connection.

--- Backend/src/config/db.ts
+++ Backend/src/config/db.ts
@@ -2,9 +2,11 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";  
 import { config } from "./envConfig";
-
+// connect to postgres database so that the prisma client will not take load on serverless servers
 const pool = new Pool({ connectionString: config.supabase.url });
+// create adapter to connect prisma to postgres
 const adapter = new PrismaPg(pool);
+// create prisma client with adapter so that 
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
